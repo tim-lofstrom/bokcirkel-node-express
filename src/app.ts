34f17b73e370e961b11ffbb4dc401c6992c5b63f
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2021-10-22 14:30:02 +0200

    chapter 1 complete

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+const pagesFolder = 'public';
+const indexPage = fs.readFileSync(path.resolve(__dirname, pagesFolder, 'index.html'));
+const homePage = fs.readFileSync(path.resolve(__dirname, pagesFolder, 'home.html'));
+const notFoundPage = fs.readFileSync(path.resolve(__dirname, pagesFolder, 'not_found.html'));
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('Hello Node!!');
+    switch (req.url) {
+        case '/':
+            res.end(indexPage);
+            break;
+        case '/home':
+            res.end(homePage);
+            break;
+        default:
+            res.writeHead(404);
+            res.end(notFoundPage);
+    }
 })
 
 server.listen(3000);
